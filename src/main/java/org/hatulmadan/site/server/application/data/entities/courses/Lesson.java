b9223fe34594abeaeb06afe2301540133e20532b
shusterboris <shuster.boris@gmail.com>
2022-03-14 09:23:59 +0200

    0.2.2.1 Добавлено поле в Lessons

--- src/main/java/org/hatulmadan/site/server/application/data/entities/courses/Lesson.java
+++ src/main/java/org/hatulmadan/site/server/application/data/entities/courses/Lesson.java
@@ -4,15 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 import java.time.ZonedDateTime;
 import org.hatulmadan.site.server.application.data.entities.BasicEntity;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.ZonedDateTime;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "LESSONS")
 public class Lesson extends BasicEntity {
+	@Column
 	private ZonedDateTime start;
+	@Column
 	private Long group;
-	private int number;
+	@Column
+	private String comment;
 }
