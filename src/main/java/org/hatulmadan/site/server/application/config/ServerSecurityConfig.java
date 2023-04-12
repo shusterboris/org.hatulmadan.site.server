1422b8fba02aa9abb9c51654af6b02be857ae275
shusterboris <shuster.boris@gmail.com>
2023-04-12 14:36:37 +0300

    1.2.2 cors settings, qa-properties

--- src/main/java/org/hatulmadan/site/server/application/config/ServerSecurityConfig.java
+++ src/main/java/org/hatulmadan/site/server/application/config/ServerSecurityConfig.java
@@ -80,7 +80,8 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000", "http://localhost","http://127.0.0.1:8080", "https://q.hatulmadan.net" , "https://hatulmadan.net"));
+                "http://localhost:3000", "http://localhost","http://127.0.0.1:8080",
+                "https://q.hatulmadan.net" ,"https://qqa.hatulmadan.net", "https://hatulmadan.net"));
         configuration.setAllowedMethods(Collections.singletonList("*"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
